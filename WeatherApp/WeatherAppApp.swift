ceb5fb7440cf9bfc1ed78ec743509d7377f955cc
shaahzaib <110594965+shaahzaib@users.noreply.github.com>
2025-12-09 12:37:30 +0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Macbook Pro on 09/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
